900edf417c4e96678d7cf0f630dfcb477c3d89b4
tireDep <wing997@naver.com>
2018-09-16 16:31:11 +0900

    - malloc이후, 배열 초기화 - 배열 크기 확인 함수 변경(malloc으로 선언한 배열은 sizeof()로 확인x -> _msize()로 변경)

--- GameofLife/Main.cpp
+++ GameofLife/Main.cpp
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include <malloc.h>
 
 void LiveorDead(int *array[]);	// 생명 체크 함수
 void IsAlive(int *array[], int lifeCnt); // 생존여부 판별 함수
@@ -23,16 +24,15 @@ int main()
 	}
 	// 2차원 배열 생성
 	
-/*	for(i=0;i<size;i++)
+	for(i=0;i<size;i++)
 	{
-		for(j=0;j<size;j++)
-		{
-			printf("%d ",field[i][j]);
-		}
-		puts("");
-	} // 초기화 확인용 */
+		for (j = 0; j < size; j++)
+			field[i][j] = 0;
+	} // 초기화
 // 격자크기 입력까지 완료됨!
-	int xgrid, ygrid; // 좌표값 변수
+
+	int xgrid = 0, ygrid = 0; // 좌표값 변수
+
 	while(1)
 	{
 		printf("생명의 위치를 입력하시오.(단, x,y형태로 입력할 것, 좌표값이 음수면 종료) >> ");
@@ -45,18 +45,15 @@ int main()
 		}
 		if(xgrid>size || ygrid>size || xgrid ==0 || ygrid == 0)
 		{
-			printf("올바른 범위가 아닙니다");
+			printf("올바른 입력방법이 아닙니다.");
+			xgrid = 0;
+			ygrid = 0;	// 에러 방지를 위한 변수 초기화
 			continue;
 		}
-		/*if(xgrid==size)
-			xgrid--;
-		else if(xgrid==1)
-			xgrid++;
-		if(ygrid==size)
-			ygrid--;
-		else if(ygrid==1)
-			ygrid++;*/
-		field[--ygrid][--xgrid] = 1;
+		field[--ygrid][--xgrid] = 1;	// 배열 범위는 0~4, 입력은 편한 이해를 위해서 1~5로 받기 때문
+
+		xgrid = 0;
+		ygrid = 0;	// 에러 방지를 위한 변수 초기화
 	}
 	// 생명 위치 입력받음(후에 수정해보기)
 	for(i=0;i<size;i++)
@@ -67,6 +64,7 @@ int main()
 		}
 		puts("");
 	}
+	// test printf("%d\n\n\n", _msize(field));
 	LiveorDead(field);
 	puts("ok");
 	
@@ -74,34 +72,34 @@ int main()
 }
 void LiveorDead(int *array[])
 {
+	int i,j;
+	int lifeCnt=0; // 생명 근처 생명 개수
+	int checkArr = _msize(array) / sizeof(array);	// 범위 체크 변수, _misze() : malloc으로 할당한 메모리 크기 구할 수 있음
+	for(i=0;i<checkArr;i++)	
+	{
+		for(j=0;j<checkArr;j++)
+		{
+				if(array[i][j]==1)
+					lifeCnt++;
+		}
+	}
+	IsAlive(array, lifeCnt);
+}
+
+void IsAlive(int *array[], int lifeCnt)
+{
 	/*
 	-11 / 01 / 11
 	-10 / 00 / 10
 	-1-1 / 0-1 / 1-1
+
+	생명 판단 규칙
+	- 3개 : 생존
+	- 2개 : 상태유지
+	- 그외 : 사망
+	=> 생명개수 -1 로 적용해야할듯? ==> 자리 판별시 본인도 판별됨
 	*/
-	int i,j;
-	int lifeCnt=0; // 생명 근처 생명 개수
-	for(i=0;i<sizeof(array)+1;i++)
-	{
-		for(j=0;j<sizeof(array)+1;j++)
-		{
-				/*
-				생명 판단 규칙
-				- 3개 : 생존
-				- 2개 : 상태유지
-				- 그외 : 사망
-				=> 생명개수 -1 로 적용해야할듯? ==> 자리 판별시 본인도 판별됨
-				*/
-				if(array[i][j]==1)
-					lifeCnt++;
-		}
-		if(i==sizeof(array)-1)
-			IsAlive(array, lifeCnt);
-	}
-	printf("함수 제작필요");
-}
-void IsAlive(int *array[], int lifeCnt)
-{
+
 	printf("%d ",lifeCnt);
-	printf("함수 제작필요");
+	printf("!!!!함수 제작필요");
 }
